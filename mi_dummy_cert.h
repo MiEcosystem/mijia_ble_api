bee9ce34e9fe93d170819d8f8b162cbe582ae094
suhang7 <suhang7@xiaomi.com>
2020-12-29 15:25:50 +0800

    add mi_dummy_cert.h for mi_mesh_otp_program_simulation

---
+++ mi_dummy_cert.h
@@ -0,0 +1,14 @@
+#ifndef MI_DUMMY_CERT_H_
+#define MI_DUMMY_CERT_H_
+
+static const uint8_t root_cert[367]={0};
+
+static const uint8_t manu_cert[402] = {0};
+
+const unsigned char dev_mac[6] = {0};
+
+static const unsigned char dev_cert[414]={0};
+
+static const unsigned char dev_cert_sk[32] = {0};
+
+#endif /* MI_DUMMY_CERT_H_ */
